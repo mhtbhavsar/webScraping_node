754b1810f27e3a125b59165e778347d574906c38
mhtbhavsar <mohit.bhavsar@coderize.in>
2017-01-28 01:51:25 +0530

    page automation

--- routes/webScrape.js
+++ routes/webScrape.js
@@ -15,6 +15,46 @@ var products = [];
 
 /* GET users listing. */
 router.post('/web/scrape', function(req, res, next) {
+
+
+    request(req.body.url, function(err, resp, body) {
+        if (err) {
+            console.log('error occured : ' + err);
+        } else {
+            var $ = cheerio.load(body);
+
+            var result = '#result_';
+
+            for (var i = 0; i < 24; i++) {
+                var productName = $(result + i + ' .s-access-title');
+                var price = $(result + i + ' .s-price');
+                var rating = $(result + i + '.a-declarative .a-icon-star .a-icon-alt');
+                var productNameText = productName.text();
+                products[i] = { "productName": productNameText, price: price.text(), rating: rating.text(), };
+            }
+
+            fs.writeFile('jsonFiles/AmazonProducts.json', JSON.stringify(products, null, 4), function(err) {
+
+                console.log('File successfully written! - Check your project directory for the output.json file');
+
+            })
+
+            Product.addProduct(products, function(err, products) {
+                if (err) {
+                    res.status(500).json({ success: false, message: "error in upgradation of Product...!", err: err });
+                } else {
+                    res.status(200).json({ success: true, message: "Products inserted successfully..!", count: products.length, data: products });
+                }
+            })
+            console.log(JSON.stringify(products));
+        }
+    })
+
+
+/* GET users listing. */
+router.post('/web/scrape/all', function(req, res, next) {
+
+    
     request(req.body.url, function(err, resp, body) {
         if (err) {
             console.log('error occured : ' + err);
